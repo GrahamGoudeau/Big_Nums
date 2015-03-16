138b04622f4ce434806c10a814b77165933c414a
Graham Goudeau <grahamgoudeau@gmail.com>
2015-03-16 00:44:36 -0400

    arbitrary precision unsigned arithmetic working

--- big_num.h
+++ big_num.h
@@ -6,15 +6,14 @@ typedef size_t num_index;
 struct big_num_s;
 typedef struct big_num_s *big_num_p;
 
-extern big_num_p init_big_num(void);
-
-/* expects a null terminating character at end of input */
+/* expects a null terminating character at end of numeric input */
 extern big_num_p parse_big_num(char *input);
 
 extern void print_big_num(big_num_p num);
 
 extern void free_big_num(big_num_p num);
 
+/* returns a new big_num_p holding result of calculation */
 extern big_num_p add(big_num_p operand1, big_num_p operand2);
 
 
